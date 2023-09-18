660d517663ef657e79fd3ba8c000036346345ab4
“moviedo” <moviedo@konecta-grupo.com>
2023-09-18 17:26:31 -0300

    './

--- src/domain/IADNService.ts
+++ src/domain/IADNService.ts
@@ -1,5 +1,5 @@
 import { IDNASchema } from './../infra/schemas/adn.schema';
-import { ValidateMutationDto, IStatsDto } from './dto';
+import { ValidateMutationDto, IStatsDto,AdnDto } from './dto';
 
 export interface IADNService {
   MutationVerify: (req: ValidateMutationDto) => Promise<boolean>;
--- src/domain/dto/index.ts
+++ src/domain/dto/index.ts
@@ -1,2 +1,3 @@
 export * from './GetStatsDto'
 export * from './ValidateMutationDto'
+export * from AdnDTO'
